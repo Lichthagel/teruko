be2696bddc77294fbb67573133b9b97be341d88d
Lichthagel <lichthagel@tuta.io>
2023-07-02 21:50:06 +0200

    tagSuggestions endpoint

--- packages/server-common/graphql/Query/index.ts
+++ packages/server-common/graphql/Query/index.ts
@@ -2,9 +2,11 @@ import { builder as schemaBuilder } from "../builder";
 import image from "./image";
 import images from "./images";
 import tag from "./tag";
+import tagSuggestions from "./tagSuggestions";
 
 export const applyQuery = (builder: typeof schemaBuilder) => {
   images(builder);
   image(builder);
   tag(builder);
+  tagSuggestions(builder);
 };
---
+++ packages/server-common/graphql/Query/tagSuggestions.ts
@@ -0,0 +1,32 @@
+import { db, dTag, dTagCategory } from "../../db";
+import { builder } from "../builder";
+import { eq, sql } from "drizzle-orm";
+import { PothosTag } from "../Tag";
+
+export default (b: typeof builder) =>
+  b.queryField("tagSuggestions", (t) =>
+    t.field({
+      type: [PothosTag],
+      args: {
+        query: t.arg.string({
+          required: true,
+        }),
+      },
+      resolve: async (parent, { query }) => {
+        const result = await db
+          .select()
+          .from(dTag)
+          .where(
+            sql`LOWER(${dTag.slug}) LIKE LOWER(${`%${query}%`}) ESCAPE '\\'`
+          )
+          .orderBy(sql`CHAR_LENGTH(${dTag.slug})`)
+          .limit(10)
+          .leftJoin(dTagCategory, eq(dTag.categorySlug, dTagCategory.slug));
+
+        return result.map((r) => ({
+          ...r.Tag,
+          category: r.TagCategory,
+        }));
+      },
+    })
+  );
