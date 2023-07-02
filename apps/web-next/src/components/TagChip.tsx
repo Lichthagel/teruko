421ace13f68e9dec7f5088f28e96896d0d1de0b3
Lichthagel <lichthagel@tuta.io>
2023-07-02 21:50:37 +0200

    next: use NextLink on image page

--- apps/web-next/src/components/TagChip.tsx
+++ apps/web-next/src/components/TagChip.tsx
@@ -1,17 +1,18 @@
 import { TagExt } from "models";
+import NextLink from "next/link";
 
 type TagChipProps = {
   tag: TagExt;
 };
 
 const TagChip: React.FC<TagChipProps> = ({ tag }: TagChipProps) => (
-  <a
+  <NextLink
     href={`/?tag=${tag.slug}`}
     className="m-0.5 inline-block select-none whitespace-nowrap rounded bg-gray-500 px-2 py-1 text-white transition hover:brightness-75"
     style={{ backgroundColor: tag.category?.color ?? undefined }}
   >
     {tag.slug}
-  </a>
+  </NextLink>
 );
 
 export default TagChip;
