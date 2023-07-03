0a497f3ceab8afe53c8ab94fd4620db2b99441e0
Lichthagel <lichthagel@tuta.io>
2023-07-03 14:49:24 +0200

    update deps

--- apps/web-nuxt/nuxt.config.ts
+++ apps/web-nuxt/nuxt.config.ts
@@ -3,6 +3,9 @@ import { NuxtConfig } from "nuxt/schema";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 const config: NuxtConfig = defineNuxtConfig({
   devtools: { enabled: true },
+  devServer: {
+    host: "0.0.0.0",
+  },
   modules: ["@nuxtjs/eslint-module", "@nuxtjs/tailwindcss"],
   plugins: ["~/plugins/urql/index.ts"],
   components: [
