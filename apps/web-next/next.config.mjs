257d6737d950e24e7f8a7dbd830a8e1cae7270c5
Lichthagel <hey@lichthagel.de>
2024-06-10 14:51:38 +0200

    fix(web-next): mark graphql as external

--- apps/web-next/next.config.mjs
+++ apps/web-next/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["client-common", "server-common", "models"],
+  experimental: {
+    serverComponentsExternalPackages: ["graphql"],
+  },
 };
 
 export default nextConfig;
