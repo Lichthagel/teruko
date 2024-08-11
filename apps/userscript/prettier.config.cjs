31a55479bc18998298dc750a96eebf59e19a3b2f
Lichthagel <hey@lichthagel.de>
2024-08-11 14:09:16 +0200

    lint(userscript): allow require in prettier config

--- apps/userscript/prettier.config.cjs
+++ apps/userscript/prettier.config.cjs
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import("prettier").Config} */
 const config = {
   // @ts-expect-error This is a valid Prettier config
