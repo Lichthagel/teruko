05696ca4702d92802d264577eaa76379c116bd5e
Lichthagel <hey@lichthagel.de>
2024-09-17 13:07:15 +0200

    chore(app-nuxt): set compatibilityDate

--- apps/web-nuxt/nuxt.config.ts
+++ apps/web-nuxt/nuxt.config.ts
@@ -2,6 +2,7 @@ import type { NuxtConfig } from "nuxt/schema";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 const config: NuxtConfig = defineNuxtConfig({
+  compatibilityDate: "2024-09-17",
   devtools: { enabled: true },
   devServer: {
     host: "0.0.0.0",
