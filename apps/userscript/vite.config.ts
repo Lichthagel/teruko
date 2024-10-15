15897154e568ce59a30b6819ae10f83965d52b58
Lichthagel <hey@lichthagel.de>
2024-10-15 23:57:27 +0200

    fix(userscript): adjust metablock

--- apps/userscript/vite.config.ts
+++ apps/userscript/vite.config.ts
@@ -1,22 +1,21 @@
 import { defineConfig } from "vite";
 import monkey from "vite-plugin-monkey";
 
-import { version } from "./package.json";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     monkey({
       entry: "src/main.ts",
+      build: {
+        fileName: "teruko.user.js",
+      },
       userscript: {
         name: "Teruko Userscript",
         namespace: "Lichthagel",
-        version,
         author: "Lichthagel",
-        description: "",
         homepageURL: "https://github.com/Lichthagel/teruko",
         supportURL: "https://github.com/Lichthagel/teruko/issues",
-        match: ["https://www.pixiv.net/*"],
+        match: ["*://www.pixiv.net/*"],
       },
     }),
   ],
