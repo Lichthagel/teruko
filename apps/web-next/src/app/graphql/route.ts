e59773a480017de289a43eaea1c54da09c893467
Lichthagel <hey@lichthagel.de>
2024-10-22 10:15:05 +0200

    fix(app-+ext): handler type

--- apps/web-next/src/app/graphql/route.ts
+++ apps/web-next/src/app/graphql/route.ts
@@ -1,12 +1,11 @@
-// eslint-disable-next-line n/no-extraneous-import
-import type {} from "@whatwg-node/server";
-
 import { getYoga } from "server-graphql";
 
 const { handleRequest } = getYoga({ Response });
 
+const handler = (req: Request) => handleRequest(req, {});
+
 export {
-  handleRequest as GET,
-  handleRequest as POST,
-  handleRequest as OPTIONS,
+  handler as GET,
+  handler as POST,
+  handler as OPTIONS,
 };
