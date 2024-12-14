de09edc2fe3e12ad16cf0fba5781cba76f897484
Lichthagel <hey@lichthagel.de>
2024-12-14 11:21:13 +0100

    refactor!(web-next): remove

--- apps/web-next/next.config.ts
+++
@@ -1,7 +0,0 @@
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  serverExternalPackages: ["graphql"],
-};
-
-export default nextConfig;
--- apps/web-next/postcss.config.mjs
+++
@@ -1,3 +0,0 @@
-// @ts-check
-
-export { default } from "client-css/postcss.config.mjs";
