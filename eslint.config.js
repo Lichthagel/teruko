1dcd4265ebf8376811ae6f7945609bddff81bc1d
Lichthagel <hey@lichthagel.de>
2025-10-18 14:06:34 +0200

    refactor: use antfu eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,82 +1,22 @@
-import lichthagel from "@lichthagel/eslint-config";
-import vue from "eslint-plugin-vue";
-import path from "node:path";
+import antfu from "@antfu/eslint-config";
 
-/** @type {import("eslint").Linter.Config[]} */
-export default [
-  ...await lichthagel({
-    browser: true,
-    node: true,
-    svelte: true,
-  }),
-  ...vue.configs["flat/recommended"],
-  {
-    languageOptions: {
-      parserOptions: {
-        extraFileExtensions: [".svelte", ".vue"],
-        projectService: {
-          allowDefaultProject: ["*.config.js", "*.config.cjs"],
-          defaultProject: path.join(import.meta.dirname, "tsconfig.eslint.json"),
-        },
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    rules: {
-      'svelte/no-goto-without-base': 'off',
-    }
+export default antfu({
+  vue: true,
+  solid: true,
+  svelte: true,
+  stylistic: {
+    quotes: "double",
+    semi: true,
   },
-  {
-    files: ["*.vue", "**/*.vue"],
-    languageOptions: {
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-      },
-      globals: {
-        computed: "readonly",
-        defineEmits: "readonly",
-        defineExpose: "readonly",
-        defineProps: "readonly",
-        onBeforeUnmount: "readonly",
-        onMounted: "readonly",
-        onUnmounted: "readonly",
-        reactive: "readonly",
-        ref: "readonly",
-        shallowReactive: "readonly",
-        shallowRef: "readonly",
-        toRef: "readonly",
-        toRefs: "readonly",
-        useHead: "readonly",
-        useRoute: "readonly",
-        useRouter: "readonly",
-        watch: "readonly",
-        watchEffect: "readonly",
-      },
-    },
-    rules: {
-      "vue/attributes-order": "off",
-      "vue/multi-word-component-names": "off",
-    },
-  },
-  {
-    files: ["packages/**/*.config?(.d).?(c|m){js,ts}"],
-    languageOptions: {
-      parserOptions: {
-        project: "./tsconfig.eslint.json",
-        projectService: false,
-      }
-    }
-  },
-  {
-    ignores: [
-      "**/node_modules/**",
-      "**/dist/**",
-      "**/.nx/**",
-      "**/.next/**",
-      "**/.svelte-kit/**",
-      "**/.output/**",
-      "**/.nuxt/**",
-      "**/build/**",
-      "eslint.config.js"
-    ],
-  },
-];
+  ignores: [
+    "**/node_modules/**",
+    "**/dist/**",
+    "**/.nx/**",
+    "**/.next/**",
+    "**/.svelte-kit/**",
+    "**/.output/**",
+    "**/.nuxt/**",
+    "**/build/**",
+    "eslint.config.js"
+  ]
+});
