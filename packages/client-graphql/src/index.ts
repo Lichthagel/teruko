ceca00e0113d28af48f26fcda3628ba8bd026b63
Lichthagel <hey@lichthagel.de>
2026-03-15 21:01:03 +0000

    fix(client-graphql): adjust cache updates

--- packages/client-graphql/src/index.ts
+++ packages/client-graphql/src/index.ts
@@ -1,6 +1,8 @@
+import type { Data } from "@urql/exchange-graphcache";
+import type { TagExt, TagRuleExt } from "models";
 import { Client, fetchExchange } from "@urql/core";
-import { cacheExchange } from "@urql/exchange-graphcache";
 
+import { cacheExchange } from "@urql/exchange-graphcache";
 import resolvers from "./resolvers/index.js";
 
 export const urqlClient = new Client({
@@ -16,11 +18,49 @@ export const urqlClient = new Client({
       resolvers,
       updates: {
         Mutation: {
+          createTagRule: (result, args, cache, _info) => {
+            const tagEntity = { __typename: "Tag", slug: (result.createTagRule as TagRuleExt).tag?.slug ?? args.tagSlug as string };
+            const otherTagEntity = { __typename: "Tag", slug: (result.createTagRule as TagRuleExt).otherTag?.slug ?? args.otherTagSlug as string };
+
+            const prevRules = cache.resolve(tagEntity, "rules");
+            if (prevRules) {
+              cache.link(tagEntity, "rules", [...(prevRules as Data[]), result.createTagRule as Data]);
+            }
+
+            const prevReferencingRules = cache.resolve(otherTagEntity, "referencingRules");
+            if (prevReferencingRules) {
+              cache.link(otherTagEntity, "referencingRules", [...prevReferencingRules as Data[], result.createTagRule as Data]);
+            }
+          },
           deleteTagRule: (_result, args, cache, _info) => {
-            cache.invalidate({
+            const id = args.id as number;
+            const ruleEntity = {
               __typename: "TagRule",
-              id: args.id as number,
-            });
+              id,
+            };
+            const ruleKey = cache.keyOfEntity(ruleEntity)!;
+
+            const tag = cache.resolve(ruleEntity, "tag") as TagExt & { __typename: string };
+            const prevRules = cache.resolve(tag, "rules") as string[] | undefined;
+            if (prevRules) {
+              cache.link(
+                tag,
+                "rules",
+                prevRules.filter(r => r !== ruleKey),
+              );
+            }
+
+            const otherTag = cache.resolve(ruleEntity, "otherTag") as TagExt & { __typename: string };
+            const prevReferencingRules = cache.resolve(otherTag, "referencingRules") as string[] | undefined;
+            if (prevReferencingRules) {
+              cache.link(
+                otherTag,
+                "referencingRules",
+                prevReferencingRules.filter(r => r !== ruleKey),
+              );
+            }
+
+            cache.invalidate(ruleEntity);
           },
         },
       },
