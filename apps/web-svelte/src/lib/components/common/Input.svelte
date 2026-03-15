8f564381fefc06159d6376e7bcfc788b2dbb6a8d
Lichthagel <hey@lichthagel.de>
2026-03-15 18:07:58 +0000

    fix(web-svelte): add Input component

---
+++ apps/web-svelte/src/lib/components/common/Input.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { HTMLInputAttributes } from "svelte/elements";
+  import styles from "client-css/m/input.module.scss";
+
+  let { value = $bindable(""), ...restProps }: HTMLInputAttributes = $props();
+
+</script>
+
+<div class={styles.container}>
+  <input type="text" bind:value={value} {...restProps} />
+</div>
