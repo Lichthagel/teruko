b0092333d1099b7a8030c79874a9417634eecb53
Lichthagel <hey@lichthagel.de>
2026-04-03 14:42:33 +0000

    fix(web-solidstart): cleanup boilerplate

--- apps/web-solidstart/src/app.tsx
+++ apps/web-solidstart/src/app.tsx
@@ -1,18 +1,19 @@
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
+import { Provider } from "@urql/solid";
+import { urqlClient } from "client-graphql";
 import { Suspense } from "solid-js";
-import "./app.css";
 
 export default function App() {
   return (
     <Router
       root={props => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
+          <Provider value={urqlClient}>
+            <Title>てる子</Title>
+            <Suspense>{props.children}</Suspense>
+          </Provider>
         </MetaProvider>
       )}
     >
