d932748e18a93a0395ad40890aa26d48e02ca4d3
Lichthagel <hey@lichthagel.de>
2026-04-06 09:42:55 +0000

    feat(web-tanstack-react): setup nitro

--- apps/web-tanstack-react/vite.config.ts
+++ apps/web-tanstack-react/vite.config.ts
@@ -1,12 +1,20 @@
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
   plugins: [
     devtools(),
     tanstackStart(),
+    nitro({
+      rolldownConfig: {
+        treeshake: {
+          moduleSideEffects: true,
+        },
+      },
+    }),
     viteReact({
       babel: {
         plugins: ["babel-plugin-react-compiler"],
