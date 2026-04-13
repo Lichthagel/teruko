be1dfdffbf13111d1f2f8d207549e37281952df1
Lichthagel <hey@lichthagel.de>
2026-04-13 08:28:50 +0000

    fix(client-graphql): add approved to UpdateTag

--- packages/client-graphql/src/snippets/mutations/UpdateTag.ts
+++ packages/client-graphql/src/snippets/mutations/UpdateTag.ts
@@ -1,17 +1,37 @@
-import type { TagExt } from "models";
+import type { Tag, TagCategory } from "models";
 import { gql } from "@urql/core";
 
-export type UpdateTagResult = { updateTag: TagExt };
+export type UpdateTagResult = {
+  updateTag: Pick<Tag, "slug" | "approved">
+    & { category?: Pick<TagCategory, "slug"> };
+};
 
-export type UpdateTagArgs = { slug: string; newSlug?: string; category?: string };
+export type UpdateTagArgs = {
+  slug: string;
+  newSlug?: string;
+  category?: string;
+  approved?: boolean;
+};
 
 export const UpdateTag = gql<UpdateTagResult, UpdateTagArgs>`
-mutation UpdateTag($slug: String!, $newSlug: String, $category: String) {
-  updateTag(slug: $slug, newSlug: $newSlug, category: $category) {
+mutation UpdateTag(
+  $slug: String!
+  $newSlug: String
+  $category: String
+  $approved: Boolean
+) {
+  updateTag(
+    slug: $slug
+    newSlug: $newSlug
+    category: $category
+    approved: $approved
+  ) {
     slug
+    approved
     category {
       slug
     }
   }
 }
+
 `;
