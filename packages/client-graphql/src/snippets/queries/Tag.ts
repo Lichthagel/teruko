396e652a0e8c9f5f44a891b7006508d6b6045e6a
Lichthagel <hey@lichthagel.de>
2026-04-13 13:36:02 +0000

    fix(client-graphql): also return slug

--- packages/client-graphql/src/snippets/queries/Tag.ts
+++ packages/client-graphql/src/snippets/queries/Tag.ts
@@ -1,17 +1,18 @@
 import type { TagCategory, Tag as TagModel } from "models";
 import { gql } from "@urql/core";
 
-export type TagResult = { tag: Pick<TagModel, "approved"> & { category: Pick<TagCategory, "color"> } };
+export type TagResult = { tag: Pick<TagModel, "slug" | "approved"> & { category: Pick<TagCategory, "color"> } };
 
 export type TagArgs = { slug: string };
 
 export const Tag = gql<TagResult, TagArgs>`
 query Tag($slug: String!) {
   tag(slug: $slug) {
+    slug
+    approved
     category {
       color
     }
-    approved
   }
 }
 `;
