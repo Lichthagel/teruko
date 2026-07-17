22eb67f01dc49b20e9c9ec125290424f6701f5c2
Lichthagel <hey@lichthagel.de>
2026-07-17 16:04:25 +0000

    chore(deps): remove solid's nitro plugin

--- apps/web-solidstart/vite.config.ts
+++ apps/web-solidstart/vite.config.ts
@@ -1,10 +1,9 @@
 import { solidStart } from "@solidjs/start/config";
-import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
 
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solidStart(), nitro()],
+  plugins: [solidStart()],
   server: {
     host: "0.0.0.0",
   },
